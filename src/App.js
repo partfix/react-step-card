310ac4ef86be6a140f6978ab88a3b485ac0ef7da
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-06-20 22:44:09 +0800

    feat: children props, component reusability

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Hey!",
+  "I'm Practicing Hooks",
+  "To be continued..",
+  "This might be the first one",
+  "Let's do limit testing",
+  "Working?"
+];
+
+
+export default function App() {
+
+  const [step, setStep] = useState(1)
+  const [thstep, setThStep] = useState(4)
+  const [isopen, setIsOpen] = useState(true)
+
+  function handleNext() {
+    if (step < 3) {                      /*updating state based on current state*/
+      setStep((curStep) => curStep + 1)
+      setThStep((curStep) => curStep + 1)
+    }
+  }
+
+  function handlePrev() {
+    if (step > 1) {
+      setStep(step - 1)
+      setThStep(thstep - 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+      <div className="close" onClick={() => setIsOpen((currOpen) => !currOpen)}>
+        &times;
+      </div>
+
+
+      {/* this is Short circuit, if this true it will show everything if flase then nothing*/}
+      {/*Don't mutate the state manually */}
+      {
+        isopen && (<div>
+          <div className="steps">
+            <div className="numbers">
+              <Condition className={step >= 1 ? "#7950f2" : ""} text="1" />
+              <Condition className={step >= 2 ? "#7950f2" : ""} text="2" />
+              <Condition className={step >= 3 ? "#7950f2" : ""} text="3" />
+            </div>
+
+
+            <div className="buttons">
+              <Button bgColor={"#7950f2"} textcolor={"#ffff"} onClick={handlePrev}><span>◀️</span>Previous</Button>
+              <Button bgColor={"#7950f2"} textcolor={"#ffff"} onClick={handleNext} >Next<span>▶️</span></Button>
+            </div>
+          </div>
+        </div>
+        )
+      }
+    </>
+  )
 }
 
-export default App;
+
+function Button({ textcolor, bgColor, onClick, children }) {
+  return (
+    <button style={{ backgroundColor: bgColor, color: textcolor }} onClick={onClick}>{children}</button>
+  )
+}
+
+function Condition({ className, text }) {
+  return (
+    <div style={{ backgroundColor: className, color: "#ffff" }}>{text}</div>
+  )
+}
+
+
+function Message({ children, step }) {
+  return (
+    <p className="message">
+      <h3>Step: {step}</h3>
+      {children}
+    </p>
+  )
+}
